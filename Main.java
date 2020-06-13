06ee886bc3da8a48e340829241ff167a31123950
kenken <kenchin005@gmail.com>
2020-06-13 14:33:00 +0900

    練習コード

---
+++ Main.java
@@ -0,0 +1,36 @@
+class Main {
+    pablic static void main (String[] args){
+
+        System.out.println("こんにちは　Java");
+    }
+
+ //↓計算
+
+    System.out.println(10);
+
+    System.out.println(3 + 2);
+
+    System.out.println("1+3");
+
+
+
+
+    // 12を3で割った値を出力してください
+    System.out.println(12 / 3);
+    
+    // 3に6を掛けた値を出力してください
+    System.out.println(3 * 6 );
+        
+    // 8を3で割った時の余りを出力してください
+    System.out.println(8 % 3);
+
+
+    //文字列の結合
+    System.out.println("こんにちは" + "Java");
+
+    System.out.println("32" + "11");
+
+
+    
+
+}
